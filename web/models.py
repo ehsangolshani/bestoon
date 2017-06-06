c19d9e62e3a73dd84374cae081eeaa00d2efe761
Ehsan <ehsanroman74@gmail.com>
2017-06-07 04:19:19 +0430

    initializing commit

---
+++ web/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
